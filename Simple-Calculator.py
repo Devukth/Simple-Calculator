5908a5f47874a34f8c2fbada4d56dd8e74408170
Coologic <78857657+Devukth@users.noreply.github.com>
2021-09-21 17:39:41 +0530

    Add files via upload

---
+++ Simple-Calculator.py
@@ -0,0 +1,14 @@
+print("Simple Calculator")
+number1 = 4
+number2 = 4
+add = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+quotient = number1 // number2
+remainder = number1 % number2
+print("Sum of",number1,"and",number2,"is",add)
+print("Difference of",number1,"and",number2,"is",diff)
+print("Product of",number1,"and",number2,"is",product)
+print("Quotient of",number1,"and",number2,"is",quotient)
+print("Remainder of",number1,"and",number2,"is",remainder)
+print("Bye!")
